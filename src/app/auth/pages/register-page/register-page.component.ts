7156ad41ce3d1d9a769e7f2219714b9c6c85cb7a
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2023-12-21 13:37:54 +0100

    Validaciones sincronas desde otra funcion

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
+    username: ['', [Validators.required, customValidators.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  })
+
+  constructor(private fb: FormBuilder) { }
+
+  isValidField(field: string) {
+    //obtener validacion desde un servicio externo
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
 }
